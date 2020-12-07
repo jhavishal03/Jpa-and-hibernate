64f1b9ff456ae77f2e7e1bdd4698eb26bf2fb676
Vishal <vishaljha.12rpvv@gmail.com>
2020-12-07 10:04:30 +0530

    Initiial Commit

---
+++ src/test/java/com/Dark/springdemojpaproject/SpringDemojpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dark.springdemojpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemojpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
